0ed9e9d258ae2c4763caa9ca250781dd37d73bc0
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-07 20:17:03 +0300

    Add automatic properties

--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -8,14 +8,18 @@ namespace LanguageFeatures.Models
     public class Product
     {
         public string Name { get; set; }
+        //У властивість по стандарту буде значення - "Watersports"
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        
 
         public static Product[] GetProducts()
         {
             Product kozak = new Product
             {
                 Name = "Kozak",
+                Category="Water kozak",
                 Price = 200M
             };
             Product lifejacket = new Product
