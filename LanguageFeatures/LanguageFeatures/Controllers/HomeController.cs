5c692c8efb87f45df9513e36a5c18ecac94c2bad
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-07 20:58:14 +0300

    Lambda function and LINQ

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -9,23 +9,7 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
-        {
-            ShoppingCart cart = new ShoppingCart
-            {
-                Products = Product.GetProducts()
-            };
-            Product[] productArray = {
-            new Product {Name = "Kayak" , Price = 275M},
-            new Product {Name = "Lifejacket" , Price = 48.95M},
-            new Product {Name = "Kayak" , Price = 10.7M},
-            new Product {Name = "Soccer ball" , Price = 19.4M}
-            };
-            decimal priceFilterTotal = productArray.Filter(p=>((p?.Price ?? 0)>=20)).TotalPrices();
-            decimal nameFilterTotal = productArray.Filter(p=>(p?.Name?[0]=='S')).TotalPrices();
-            return View("Index", new string[] {
-                $"Price total:{priceFilterTotal:C2}",
-                $"Name total:{nameFilterTotal:C2}"});
-        }
+        public ViewResult Index()=>
+            View(Product.GetProducts().Select(p => p?.Name));
     }
 }
