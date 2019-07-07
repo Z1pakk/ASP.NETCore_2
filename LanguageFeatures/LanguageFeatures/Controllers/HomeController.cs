32aebfb8f1d6baa0e6641088db5e70daccc04cd5
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-07 20:20:36 +0300

    Change to interpolation string.

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -27,8 +27,7 @@ namespace LanguageFeatures.Controllers
                 string name = p?.Name ?? "<No Name>";
                 decimal? price = p?.Price ?? 0;
                 string relatedName = p?.Related?.Name ?? "<None>";
-                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", 
-                    name, price, relatedName));
+                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
             }
             return View(results);
 
