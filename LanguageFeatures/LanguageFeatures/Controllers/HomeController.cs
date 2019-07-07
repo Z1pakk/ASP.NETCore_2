389391ac600ea091e094649ed13ab7baa53c0eec
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-07 20:09:41 +0300

    Edit controller to null-if operation

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LanguageFeatures.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanguageFeatures.Controllers
@@ -10,7 +11,26 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            return View(new string[] {"C#","Language","Features" });
+            //Традиционно работа со значениями null требовала явных 
+            //проверок, которые могли становиться
+            //утомительными и подверженными ошибкам, когда требовалось
+            //инспектировать и объект, и его свойства.За счет 
+            //применения null - условной операции такие проверки
+            //будут намного легче и компактнее
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                //Если значение р равно
+                //null, то переменная name также будет установлена в 
+                //null.Если значение р не равно null, тогда переменной 
+                //name будет присвоено значение свойства Person.Name
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                results.Add(string.Format("Name: {0}, Price:{1}", name, price));
+            }
+            return View(results);
+
+            //return View(new string[] {"C#","Language","Features" });
         }
     }
 }
