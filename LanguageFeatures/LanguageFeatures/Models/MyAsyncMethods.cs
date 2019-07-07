3bc9e765be3ce0ba8f3bc92c09e80fa28dcebff4
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-07 21:11:21 +0300

    async await

--- LanguageFeatures/LanguageFeatures/Models/MyAsyncMethods.cs
+++ LanguageFeatures/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -8,16 +8,13 @@ namespace LanguageFeatures.Models
 {
     public class MyAsyncMethods
     {
-        public static Task<long?> GetPageLenght()
+        public async static Task<long?> GetPageLenght()
         {
             HttpClient client = new HttpClient();
 
-            var httpTask = client.GetAsync("http://google.com.ua");
+            var httpMessage = await client.GetAsync("http://google.com.ua");
 
-            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
-            {
-                return antecedent.Result.Content.Headers.ContentLength;
-            });
+            return httpMessage.Content.Headers.ContentLength;
         }
     }
 }
