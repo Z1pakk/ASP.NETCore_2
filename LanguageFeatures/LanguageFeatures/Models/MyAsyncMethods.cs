adc25063684fb65f14d35d050ffcf73800b93dc4
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-07 21:08:19 +0300

    Async method

---
+++ LanguageFeatures/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        public static Task<long?> GetPageLenght()
+        {
+            HttpClient client = new HttpClient();
+
+            var httpTask = client.GetAsync("http://google.com.ua");
+
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+            {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+    }
+}
