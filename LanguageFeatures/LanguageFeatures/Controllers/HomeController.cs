de5de57e8efd283d328c2826f6ef24d60308a110
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-07 21:19:45 +0300

    Nameof

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -9,7 +9,23 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()=>
-            View(Product.GetProducts().Select(p => p?.Name));
+        public ViewResult Index()
+        {
+            var products = new[]
+            {
+              new {Name="Kayak",Price=275M},
+              new {Name="Lifejacket",Price=45.2M},
+              new {Name="Soccer ball",Price=20.50M},
+              new {Name="Corner flag",Price=45.95M}
+          };
+            //Вивід за допомогою Select. Но тут одна проблема, якщо ми змінимо
+            //назву властивості то і строку, що показує юзеру інформацію,
+            //також потрыбно змынювати.
+            //return View(products.Select(р => $"Name: {р. Name} , Price: {р. Price} "));
+            
+            //Краще покласти цю роботу на компілятор за допомогою nameof()
+            //Якщо ми поміняємо назву зміною то нам не треба буде змінювати строку.
+            return View(products.Select(p => $"{nameof(p.Name)}: {p.Name} , {nameof(p.Price)}: {p.Price} "));
+        }
     }
 }
