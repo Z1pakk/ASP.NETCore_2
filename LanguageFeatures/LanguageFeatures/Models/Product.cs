841e431daecd867ba4b4abf865dc8bcc80fb7d92
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-07 19:52:43 +0300

    Add new model Product.

---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product kozak = new Product
+            {
+                Name = "Kozak",
+                Price = 200M
+            };
+            Product lifejacket = new Product
+            {
+                Name = "Lifejacket",
+                Price = 50.98M
+            };
+            return new Product[] { kozak, lifejacket, null };
+        }
+    }
+}
